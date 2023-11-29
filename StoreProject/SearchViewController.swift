8abd8efbece0125f41489aa350203120184937ff
€%€R Padilla! <eder.padilla97@gmail.com>
2023-11-28 19:58:41 -0600

    add cells

--- StoreProject/SearchViewController.swift
+++ StoreProject/SearchViewController.swift
@@ -11,9 +11,11 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    var searchResults = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right: 0)
         // Do any additional setup after loading the view.
     }
     
@@ -23,19 +25,36 @@ class SearchViewController: UIViewController {
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is: '\(searchBar.text!)'")
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(
+                String(
+                    format: "Fake Result %d for '%@'", i, searchBar.text!
+                )
+            )
+        }
+        tableView.reloadData()
     }
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        0
+        searchResults.count
     }
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell = tableView.dequeueReusableCell(
+            withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell?.textLabel!.text = searchResults[indexPath.row]
+        return cell ?? UITableViewCell()
     }
 }
 
