b6b1e099cf4ae2b4769cda598dc234297b1e444b
€%€R Padilla! <eder.padilla97@gmail.com>
2023-12-12 20:10:09 -0600

    Add sorting

--- StoreProject/SearchResult.swift
+++ StoreProject/SearchResult.swift
@@ -61,7 +61,21 @@ class SearchResult: Codable, CustomStringConvertible {
     }
     
     var type: String {
-        kind ?? "audiobook"
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: break
+        }
+        return "Unknown"
     }
     
     var artist: String {
