cc73d9a9ebc9b14bf07cdcdd16a7f963566c5028
€%€R Padilla! <eder.padilla97@gmail.com>
2023-12-12 19:40:04 -0600

    add model parser and more fields

--- StoreProject/SearchResult.swift
+++ StoreProject/SearchResult.swift
@@ -12,11 +12,22 @@ class ResultArray: Codable {
     var results = [SearchResult]()
 }
 
-class SearchResult: Codable {
+class SearchResult: Codable, CustomStringConvertible {
     var artistName: String? = ""
     var trackName: String? = ""
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency: String? = ""
+    var artworkUrl60: String? = ""
+    var artworkUrl100: String? = ""
+    var trackViewUrl: String? = ""
+    var primaryGenreName: String? = ""
     
     var name: String {
         return trackName ?? ""
+    }
+    
+    var description: String {
+        "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 }
