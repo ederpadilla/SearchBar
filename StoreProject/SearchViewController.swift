de4bcbaec9a14fba55effc146cbdaa5162a4d19d
€%€R Padilla! <eder.padilla97@gmail.com>
2023-12-12 20:04:57 -0600

    fix cell

--- StoreProject/SearchViewController.swift
+++ StoreProject/SearchViewController.swift
@@ -116,7 +116,14 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             
             let searchResult = searchResults[indexPath.row]
             cell.nameLabel.text = searchResult.name
-            cell.artistNameLabel.text = searchResult.artistName
+            if searchResult.artist.isEmpty {
+                cell.artistNameLabel.text = "Unknown"
+            } else {
+                cell.artistNameLabel.text = String(
+                    format: "%@ (%@)",
+                    searchResult.artist,
+                    searchResult.type)
+            }
             return cell
         }
     }
