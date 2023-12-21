e1aa2cac6dc95fb85bea2f7f7615f6df68eb18ce
€%€R Padilla! <eder.padilla97@gmail.com>
2023-12-21 16:12:28 -0600

    Add threads

--- StoreProject/SearchViewController.swift
+++ StoreProject/SearchViewController.swift
@@ -29,16 +29,6 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
     
-    func performStoreRequest(with url: URL) -> Data? {
-        do {
-            return try Data(contentsOf:url)
-        } catch {
-            print("Download Error: \(error.localizedDescription)")
-            showNetworkError()
-            return nil
-        }
-    }
-    
     func parse(data: Data) -> [SearchResult] {
         do {
             let decoder = JSONDecoder()
@@ -82,21 +72,29 @@ extension SearchViewController: UISearchBarDelegate {
             hasSearched = true
             searchResults = []
             
-            let queue = DispatchQueue.global()
-            let url = self.iTunesURL(searchText: searchBar.text!)
-            queue.async {
-                if let data = self.performStoreRequest(with: url) {
-                    self.searchResults = self.parse(data: data)
-                    self.searchResults.sort(by: <)
-                    
-                    DispatchQueue.main.async {
-                        self.isLoading = false
-                        self.tableView.reloadData()
+            let url = iTunesURL(searchText: searchBar.text!)
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url) {data, response, error in
+                if let error = error {
+                    print("Failure! \(error.localizedDescription)")
+                } else if let httpResponse = response as? HTTPURLResponse,
+                          httpResponse.statusCode == 200 {
+                    if let data = data {
+                        print("On main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
+                        self.searchResults = self.parse(data: data)
+                        self.searchResults.sort(by: <)
+                        DispatchQueue.main.async {
+                            print("On main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
+                            self.isLoading = false
+                            self.tableView.reloadData()
+                        }
+                        return
                     }
-                    
-                    return
+                } else {
+                    print("Failure! \(response!)")
                 }
             }
+            dataTask.resume()
         }
     }
     
